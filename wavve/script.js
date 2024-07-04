faba8b0c9aeb6a0abb70e547416a109682e288ed
hd9886 <90983464+hd9886@users.noreply.github.com>
2024-07-04 21:20:36 +0900

    Add files via upload

---
+++ wavve/script.js
@@ -0,0 +1,64 @@
+const items = document.querySelectorAll("#card_items li");
+
+items.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.style.transform = "translateY(-20px)";
+    item.style.transition = "all 0.3s";
+  });
+  item.addEventListener("mouseout", () => {
+    item.style.transform = "translateY(0)";
+  });
+});
+
+const gnbLi = document.querySelectorAll(".topNav > li");
+gnbLi.forEach((li) => {
+  li.addEventListener("mouseover", () => {
+    const lnb = li.querySelector(".lnb");
+    const aTag = li.querySelector("a");
+    if (lnb) {
+      lnb.style.maxHeight = lnb.scrollHeight + "px";
+      lnb.style.opacity = "1";
+      aTag.classList.add("active");
+    }
+  });
+  li.addEventListener("mouseout", () => {
+    const lnb = li.querySelector(".lnb");
+    const aTag = li.querySelector("a");
+    if (lnb) {
+      lnb.style.maxHeight = "0";
+      lnb.style.opacity = "0";
+      aTag.classList.remove("active");
+    }
+  });
+});
+
+// background image change
+
+const bgImgs = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg"];
+
+const bgImg = document.querySelector("#background_img");
+bgImg.style.backgroundImage = `radial-gradient(circle, transparent, rgba(0, 0, 0, 0.7)), url(./img/${bgImgs[0]})`;
+
+const topContents = document.querySelector("#top_contents");
+const contentTit = topContents.querySelector(".top_contents_title");
+const contentDesc = topContents.querySelector(".top_contents_desc");
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    const [firstData, ...otherData] = jsonData.data;
+    console.log(firstData);
+
+    contentTit.innerText = firstData.title;
+    contentDesc.innerText = firstData.description;
+
+    items.forEach((item, index) => {
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+        const { title, description } = jsonData.data[index];
+        bgImg.style.backgroundImage = `radial-gradient(circle, transparent, rgba(0, 0, 0, 0.7)), url(./img/${bgImgs[index]})`;
+        contentTit.innerText = title;
+        contentDesc.innerText = description;
+      });
+    });
+  });
